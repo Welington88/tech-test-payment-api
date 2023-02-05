b26868d2646abb8d2d59a4f8fc20a47e2e861f5d
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2023-02-05 11:41:33 -0300

    Ajuste no teste autofixture

--- src/ApiPayment.Test/ApiPaymentTestListVenda.cs
+++ src/ApiPayment.Test/ApiPaymentTestListVenda.cs
@@ -4,6 +4,7 @@ using ApiPayment.CC.Dto.ViewModels;
 using ApiPayment.Data.Contexts;
 using ApiPayment.Domain.Repositories;
 using ApiPayment.Test.DataBase;
+using AutoFixture;
 
 namespace ApiPayment.Test;
 
@@ -15,7 +16,7 @@ public class ApiPaymentTestListVenda : IClassFixture<DbFixture>
     protected ILogger<VendaService> _logger;
     private ServiceProvider _serviceProvider;
     protected Context db;
-    
+    protected List<VendaViewModel> listaVendasFixture = new List<VendaViewModel>();
 
     public ApiPaymentTestListVenda(DbFixture fixture)
     {
@@ -26,10 +27,10 @@ public class ApiPaymentTestListVenda : IClassFixture<DbFixture>
         db.Database.EnsureCreated();
 
         _configuration = this.InitConfiguration();
-        var retorno = ListaVendas();
+        listaVendasFixture = ListaVendas().Result;
 
         _vendaResositoryMock = new Mock<IVendaRepository>();
-        _vendaResositoryMock.Setup(m => m.Get()).Returns(retorno);
+        _vendaResositoryMock.Setup(m => m.Get()).Returns(Task.FromResult(listaVendasFixture));
 
         _vendaService = new VendaService(_configuration, _vendaResositoryMock.Object, _logger);
     }
@@ -46,22 +47,22 @@ public class ApiPaymentTestListVenda : IClassFixture<DbFixture>
     //Act
     public async Task<List<VendaViewModel>> ListaVendas(){
 
-        var listaJson = "[\n  {\n    \"numeroPedido\": 5,\n    \"data\": \"2023-01-07T22:58:32.529\",\n    \"vendedor\": {\n      \"id\": 4,\n      \"cpf\": \"000.000.000-00\",\n      \"nome\": \"Nome Vendedor\",\n      \"email\": \"email@live.com\",\n      \"telefone\": \"99 99999 9999\"\n    },\n    \"itens\": [\n      {\n        \"produto\": \"Iphone X\",\n        \"quantidade\": 3,\n        \"valorUnitario\": 3401.12\n      }\n    ],\n    \"status\": \"PagamentoAprovado\"\n  },\n  {\n    \"numeroPedido\": 6,\n    \"data\": \"2023-01-08T02:39:48.621\",\n    \"vendedor\": {\n      \"id\": 5,\n      \"cpf\": \"000000000\",\n      \"nome\": \"Nome Vendedor\",\n      \"email\": \"email@live.com\",\n      \"telefone\": \"99 99999 9999\"\n    },\n    \"itens\": [\n      {\n        \"produto\": \"Notebook Asus\",\n        \"quantidade\": 1,\n        \"valorUnitario\": 2756.57\n      }\n    ],\n    \"status\": \"AguardandoPagamento\"\n  }\n]";
-        var retorno = JsonSerializer.Deserialize<List<VendaViewModel>>(listaJson);
+        var autoFixture = new Fixture();
+        autoFixture.RepeatCount = 2;
+        var venda = autoFixture.Create<VendaViewModel>();
+        var listaVendas = autoFixture.CreateMany<VendaViewModel>();
 
-        return await Task.FromResult(retorno);
+        return await Task.FromResult(listaVendas.ToList());
     }
 
     [Fact(DisplayName = "ListarVendas")]
     public void BuscarVendas()
     {
         //Arrange
-        var listaJson = "[\n  {\n    \"numeroPedido\": 5,\n    \"data\": \"2023-01-07T22:58:32.529\",\n    \"vendedor\": {\n      \"id\": 4,\n      \"cpf\": \"000.000.000-00\",\n      \"nome\": \"Nome Vendedor\",\n      \"email\": \"email@live.com\",\n      \"telefone\": \"99 99999 9999\"\n    },\n    \"itens\": [\n      {\n        \"produto\": \"Iphone X\",\n        \"quantidade\": 3,\n        \"valorUnitario\": 3401.12\n      }\n    ],\n    \"status\": \"PagamentoAprovado\"\n  },\n  {\n    \"numeroPedido\": 6,\n    \"data\": \"2023-01-08T02:39:48.621\",\n    \"vendedor\": {\n      \"id\": 5,\n      \"cpf\": \"000000000\",\n      \"nome\": \"Nome Vendedor\",\n      \"email\": \"email@live.com\",\n      \"telefone\": \"99 99999 9999\"\n    },\n    \"itens\": [\n      {\n        \"produto\": \"Notebook Asus\",\n        \"quantidade\": 1,\n        \"valorUnitario\": 2756.57\n      }\n    ],\n    \"status\": \"AguardandoPagamento\"\n  }\n]";
-        var listaVendas = JsonSerializer.Deserialize<List<VendaViewModel>>(listaJson);
         var listaVendasAssicrona = _vendaService.Get();
         var result = listaVendasAssicrona.Result.Count();
         //Assert
-        Assert.Equal(listaVendas.Count, result);
+        Assert.Equal(listaVendasFixture.Count, result);
         Assert.IsType<List<VendaViewModel>>(listaVendasAssicrona.Result);
     }
 }
